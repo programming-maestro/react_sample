c4637eb9f6dcfbd3d9573400f963e5c1c2afbf32
programming-maestro <csm.exclusive@gmail.com>
2025-12-03 01:24:29 +0530

    updating jsx with variables.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
+
+let language = "JavaScript";
+let moon = "ú";
 function App() {
 
   return (
-    <h1>Hello React!</h1>
+    <parent>
+      <h1>Hello React!</h1>
+      <h2>Vairable value is, {language}!!</h2>
+      <h2>Vairable value is, {language.toUpperCase()}!!</h2>
+      <h3>{moon}</h3>
+    </parent>
   );
 }
 
