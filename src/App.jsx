18f4bbbf064331d703e4fe10835992d9861ffa85
programming-maestro <csm.exclusive@gmail.com>
2025-12-03 01:58:41 +0530

    props passing.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import "./App.css";
 let language = "JavaScript";
 let moon = "ú";
 
-function Header() {
+function Header(props) {
   return (
     <parent>
       <h1>Hello React!</h1>
       <h2>Vairable value is, {language}!!</h2>
       <h2>Vairable value is, {language.toUpperCase()}!!</h2>
       <h3>{moon}</h3>
+
+      {/* Properties, props */}
+      <div>
+        <h1> {props.name}'s Application </h1>
+        <h3>Copyright {props.year} -- from props</h3>
+
+        <h3>Copyright {new Date().getFullYear()} -- through date function </h3>
+      </div>
+
+
     </parent>
   );
 }
@@ -19,7 +29,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header name="Chetan" year={2025} />
       <main>
         <h1>Make your complex tasks easy with "DoIT"</h1>
       </main>
